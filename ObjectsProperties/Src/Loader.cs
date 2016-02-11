ce17a7bfffd2c4cdd9274af713af4801f1c5f5f7
Sugz <plantec.clement@gmail.com>
2016-02-11 17:13:38 +0100

    Update to version 0.10 - Switch the View to a custom UserControl that enable resizing as a toolbar inside 3ds Max

--- ObjectsProperties/Src/Loader.cs
+++ ObjectsProperties/Src/Loader.cs
@@ -11,19 +11,40 @@ namespace ObjectsProperties.Src
     public class Loader : CuiDockableContentAdapter
     {
 
+        #region Fields
+
+        // Create an instance of the view to be able to modify it in this class
+        private ObjectsPropertiesView objectsPropertiesView = new ObjectsPropertiesView();
+
+
+        #endregion
+
+
+        #region Properties
+
         public override string ActionText { get { return "Objects Properties"; } }
-
         public override string Category { get { return "SugzTools"; } }
-
         public override string WindowTitle { get { return "Objects Properties"; } }
-
         public override Type ContentType { get { return typeof(ObjectsPropertiesView); } }
-
-        public override object CreateDockableContent() { return new ObjectsPropertiesView(); }
-
         public override DockStates.Dock DockingModes { get { return DockStates.Dock.Left | DockStates.Dock.Floating; } }
-
         public override bool IsMainContent { get { return true; } }
+
+        #endregion
+
+
+        #region Methods
+
+        public override object CreateDockableContent() { return objectsPropertiesView; }
+
+
+        // Modify the view based on the dockMode
+        public override void SetContentDockMode(object dockableContent, DockStates.Dock dockMode)
+        {
+            base.SetContentDockMode(dockableContent, dockMode);
+            objectsPropertiesView.SetResizeBorders(dockMode);
+        }
+
+        #endregion
 
 
     }
--- ObjectsProperties/Views/ObjectsPropertiesView.xaml.cs
+++ ObjectsProperties/Views/ObjectsPropertiesView.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UiViewModels.Actions;
 
 namespace ObjectsProperties.Views
 {
@@ -20,10 +22,94 @@ namespace ObjectsProperties.Views
     /// </summary>
     public partial class ObjectsPropertiesView : UserControl
     {
+        // Fields
+        #region Fields
+
+
+        private Grid leftBorder;
+        private Grid rightBorder;
+        private Thumb leftResizer;
+        private Thumb rightResizer;
+
+
+        #endregion // End Fields
+
+
+        // Initialisation
+        #region Initialisation
+
+
         public ObjectsPropertiesView()
         {
             InitializeComponent();
         }
 
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            leftBorder = (Grid)GetTemplateChild("LeftBorder");
+            rightBorder = (Grid)GetTemplateChild("RightBorder");
+            leftResizer = (Thumb)GetTemplateChild("LeftResizer");
+            rightResizer = (Thumb)GetTemplateChild("RightResizer");
+
+            leftResizer.DragDelta += Resizer_DragDelta;
+            rightResizer.DragDelta += Resizer_DragDelta;
+        }
+
+
+        #endregion // End Initialisation
+
+
+        // Methods
+        #region Methods
+
+        // Private Methods
+        #region Private
+
+
+        private void Resizer_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            // Set the new width depending on which thumb trigger the resize and make sure to limit the resize to the MinWidth
+            double xAdjust = ((Thumb)sender) == rightResizer ? Width + e.HorizontalChange : Width - e.HorizontalChange;
+            Width = (ActualWidth + xAdjust) > MinWidth ? xAdjust : MinWidth;
+        }
+
+
+        #endregion // End Private Methods
+
+        // Public Methods
+        #region Public
+
+
+        public void SetResizeBorders(DockStates.Dock dockMode)
+        {
+            switch (dockMode)
+            {
+                case DockStates.Dock.Floating:
+                    leftBorder.Visibility = Visibility.Collapsed;
+                    rightBorder.Visibility = Visibility.Collapsed;
+                    break;
+                case DockStates.Dock.Left:
+                    leftBorder.Visibility = Visibility.Visible;
+                    rightBorder.Visibility = Visibility.Visible;
+                    leftResizer.Visibility = Visibility.Collapsed;
+                    rightResizer.Visibility = Visibility.Visible;
+                    break;
+                case DockStates.Dock.Right:
+                    leftBorder.Visibility = Visibility.Visible;
+                    rightBorder.Visibility = Visibility.Visible;
+                    leftResizer.Visibility = Visibility.Visible;
+                    rightResizer.Visibility = Visibility.Collapsed;
+                    break;
+            }
+        }
+
+
+        #endregion // End Public Methods
+
+
+        #endregion // End Methods
     }
 }
